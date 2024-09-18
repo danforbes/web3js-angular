bd1ed927f8429bae8864f6b223fbe485ecbd6fdb
Dan Forbes <dan.forbes@chainsafe.io>
2024-09-18 17:43:58 -0400

    Fix AccountService Test Import

--- src/app/web3/accounts.service.spec.ts
+++ src/app/web3/accounts.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AccountsService } from '../web3/accounts.service';
+import { AccountsService } from './accounts.service';
 
 describe('AccountsService', () => {
   let service: AccountsService;
